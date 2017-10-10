320be56f83f6ec318cec3558c652cc3c4fc578a6
JoshLorenzen <joshlorenzen49@gmail.com>
2017-10-10 12:53:12 -0700

    Added phonebook

---
+++ PhoneBook.java
@@ -0,0 +1,44 @@
+
+import java.util.HashMap;
+
+public class PhoneBook
+{
+
+    HashMap<Integer, Contact> contacts;
+
+    
+    public PhoneBook()
+    {
+        this.contacts = new HashMap<Integer, Contact>();
+    }
+    
+    private Contact getContact(int number)
+    {
+    	return contacts.get(number);
+    }
+    
+    private Contact getContact(String name)
+    {
+    	for (Contact contact : contacts.values())
+    	{
+    		if (contact.getName().equals(name))
+    		{
+    			return contact;
+    		}
+    		throw new RuntimeException("Contact not found.");
+    	}
+    }
+
+
+    public void addContact(Integer number, Contact contact)
+    {
+        this.contacts.put(number, contact);
+    }
+
+    public void displayContact(String name)
+    {
+    	System.out.println(getContact(name));
+    }
+
+}
+
