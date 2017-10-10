2fb9aab8f6f118c7c27ec07c661e8d11b8127d03
JoshLorenzen <joshlorenzen49@gmail.com>
2017-10-10 12:51:45 -0700

    Initial commit

---
+++ Contact.java
@@ -0,0 +1,55 @@
+
+
+public class Contact {
+    
+    private String name;
+    private int phoneNumber;
+    private String email;
+    private String notes;
+        
+    public Contact(String name, int phoneNumber, String email, String notes) 
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.notes = notes;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+    this.name = name;
+    }
+    
+    public int getPhoneNumber()
+    {
+    return this.phoneNumber;
+    }
+    
+    public void setPhoneNumber(int phoneNumber)
+    {
+    this.phoneNumber = phoneNumber;
+    }
+    
+    public String email()
+    {
+    return this.email;
+    }
+public String getNotes()
+    {
+    return this.notes;
+    }
+    
+    public void setNotes(String notes)
+    {
+    this.notes = notes;
+    }
+}
+    
+
+
+
