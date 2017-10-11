aabdba3396cd1c27039f34a30b2f4e8915870e2d
Luca Guarro <luca.guarro@gmail.com>
2017-10-10 19:17:07 -0700

    dank main

--- Main.java
+++ Main.java
@@ -1,19 +1,58 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+	static Scanner reader = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		String inputLvl1 = "";
-		// TODO Auto-generated method stub
+		showMainMenu();
+		reader.close();
+	}
+
+	private static void showMainMenu() {
+		boolean endProgram = false;
+		String input = "";
 		System.out.println("Welcome to your phone. Enter a 10-digit phonenumber, name, or"
 				+ " 1-5 to choose a preset favorite");
 		System.out.println("Enter \"options\" to do other stuff. At any time type \"DONE\" to exit");
 		do {
-			inputLvl1 = reader.next();
+			input = reader.next();
+			if(input.equals("options")) {
+				endProgram = showOptions();
+			}
 			
-			
-		}while(inputLvl1 != "DONE");
+		}while(!input.equals("DONE") || endProgram);
+	}
+
+	private static boolean showOptions() {
+		 //this boolean is used for the do-while loop
+		String input = "";
+		System.out.println("Please choose an option");
+		String[] options = new String[]{"1: Get Call History","2: Edit Phonebook","3: Edit Favorites", "4: Go Back"};
+		System.out.println(Arrays.toString(options));
+		input = reader.next();
+		boolean inputValid;
+		do {
+			inputValid = true;
+			if(input.equals("DONE")) {
+				return true;
+			}
+			else if(input.equals("1")) {
+				
+			}
+			else if(input.equals("2")) {
+				
+			}
+			else if(input.equals("3")) {
+				
+			}
+			else if(input.equals("4")) {
+				return false;
+			}
+			else {
+				inputValid = false;
+			}
+		}while(!inputValid);
+		return false;
 	}
 
 }
